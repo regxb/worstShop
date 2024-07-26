d49148d2da5716795db37da630aea444519d0e31
qwe <aleksey18003@yandex.ru>
2024-07-26 14:53:48 +0300

    add shop/views: product_view, product_detail_view, category_list

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from shop.models import Product, ProductProxy, Category
+
 
 # Create your views here.
+def product_view(request):
+    products = ProductProxy.objects.all()
+    return render(request, 'shop/products.html', {"products": products})
+
+
+def product_detail_view(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'shop/product_detail.html', {'product': product})
+
+
+def category_list(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = ProductProxy.objects.select_related('category').filter(category=category)
+    context = {'products': products, 'category': category}
+    return render(request, 'shop/category_list.html', context)

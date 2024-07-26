90d2bec7a3f3f5934ad7f57b74bb4e02d7c522f1
qwe <aleksey18003@yandex.ru>
2024-07-26 12:29:14 +0300

    add model Category

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+import random
+import string
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField('Категория', max_length=250)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
+    slug = models.SlugField('URL', max_length=250, unique=True, null=False, editable=True)
+    created_at = models.DateTimeField('Время создания', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        unique_together = (['slug', 'parent'])
+
+    def __str__(self):
+        full_path = [self.name]
+        k = self.parent
+        while k is not None:
+            full_path.append(k.name)
+            k = k.parent
+        return ' > '.join(full_path[::-1])
+
+    @staticmethod
+    def _rand_slug():
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self._rand_slug() + '-pickBetter' + self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('shop:category-list', args=[self.slug])

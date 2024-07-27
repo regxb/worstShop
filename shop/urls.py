71cb735da90900274772c0b9d97b374cbaa80721
qwe <aleksey18003@yandex.ru>
2024-07-27 03:14:35 +0300

    update shop/urls

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,6 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', products_view, name='products'),
-    path('<slug:slug>/', product_detail_view, name='product_detail'),
+    path('<slug:slug>/', product_detail_view, name='product-detail'),
     path('search/<slug:slug>', category_list, name='category_list'),
 ]

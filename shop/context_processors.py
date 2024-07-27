0026c830e9526d33b2eaf2fcc778c43afbdf8a79
qwe <aleksey18003@yandex.ru>
2024-07-27 03:14:17 +0300

    update shop/context_processors.py

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -2,5 +2,5 @@ from .models import Category
 
 
 def categories(request):
-    categories = Category.objects.all()
+    categories = Category.objects.filter(parent=None)
     return {'categories': categories}

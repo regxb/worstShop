197dd70cfb7cdcd19acd1dc1850c0a87ac88950e
qwe <aleksey18003@yandex.ru>
2024-07-29 13:35:13 +0300

    update shop/models

--- shop/models.py
+++ shop/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', blank=True, null=True)
     slug = models.SlugField('URL', max_length=250, unique=True, null=False, editable=True)
     created_at = models.DateTimeField('Время создания', auto_now_add=True)
+    image = models.ImageField('Изображение', upload_to='products/products/%Y/%m/%d', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Категория'
@@ -29,22 +30,22 @@ class Category(models.Model):
     def _rand_slug():
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))
 
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self._rand_slug() + '-pickBetter' + self.name)
-        super(Category, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.slug:
+    #         self.slug = slugify(self._rand_slug() + '-pickBetter' + self.name)
+    #     super(Category, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return reverse('shop:category-list', args=[self.slug])
+        return reverse('shop:products', args=[self.slug])
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ManyToManyField(Category, related_name='products')
     title = models.CharField('Название', max_length=250)
     brand = models.CharField('Бренд', max_length=250)
     description = models.TextField('Описание', blank=True)
     slug = models.SlugField('URL', max_length=250)
-    price = models.DecimalField('Цена', max_digits=7, decimal_places=2, default=99.99)
+    price = models.DecimalField('Цена', max_digits=7, decimal_places=0, default=99)
     image = models.ImageField('Изображение', upload_to='products/products/%Y/%m/%d')
     available = models.BooleanField('Наличие', default=True)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
@@ -58,7 +59,7 @@ class Product(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("shop:product-detail", args=[str(self.slug)])
+        return reverse("shop:product_detail", args=[str(self.slug)])
 
 
 class ProductManager(models.Manager):

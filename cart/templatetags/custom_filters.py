19b9eead25daab7178291fe080924e0542665cd2
qwe <aleksey18003@yandex.ru>
2024-08-01 02:27:56 +0300

    update custom_filters.py

---
+++ cart/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+# myapp/templatetags/custom_filters.py
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def format_number(value):
+    try:
+        return '{:,}'.format(value).replace(',', ' ')
+    except (ValueError, TypeError):
+        return value
+
+
+@register.filter
+def pluralize(value):
+    if value % 10 == 1 and value % 100 != 11:
+        return "товар"
+    elif value % 10 in [2, 3, 4] and (value % 100 < 10 or value % 100 >= 20):
+        return "товара"
+    else:
+        return "товаров"

3bfcd158a7c7019134d175ba14af48b84c9593dd
qwe <aleksey18003@yandex.ru>
2024-08-01 02:26:10 +0300

    update cart.views

--- cart/views.py
+++ cart/views.py
@@ -15,5 +15,31 @@ def cart_add(request):
         product_id = request.POST.get('product_id')
         product = get_object_or_404(ProductProxy, id=product_id)
         cart.add(product=product)
-        context = {'product': product}
-        return render(request, 'cart/cart.html', context)
+        new_quantity = cart.__len__()
+        new_price = cart.get_total_price()
+        response = JsonResponse({'quantity': new_quantity, 'price': new_price})
+        return response
+
+
+def cart_delete(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product = get_object_or_404(ProductProxy, id=product_id)
+        cart.delete(product)
+        new_quantity = cart.__len__()
+        new_price = cart.get_total_price()
+        response = JsonResponse({'quantity': new_quantity, 'price': new_price})
+        return response
+
+
+def cart_remove(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product = get_object_or_404(ProductProxy, id=product_id)
+        cart.remove(product=product)
+        new_quantity = cart.__len__()
+        new_price = cart.get_total_price()
+        response = JsonResponse({'quantity': new_quantity, 'price': new_price})
+        return response

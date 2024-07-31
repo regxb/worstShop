b474a0cd075955866f6984feb7001d4c4634508e
qwe <aleksey18003@yandex.ru>
2024-08-01 02:26:04 +0300

    update cart.urls

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
-    path('cart_add', views.cart_add, name='cart_add'),
+    path('add', views.cart_add, name='cart_add'),
+    path('delete', views.cart_delete, name='cart_delete'),
+    path('remove', views.cart_remove, name='cart_remove'),
 ]

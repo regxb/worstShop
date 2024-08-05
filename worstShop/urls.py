e3949fb64e60d66dededa10eaf02b09d0d51f079
qwe <aleksey18003@yandex.ru>
2024-08-05 16:55:09 +0300

    add users path

--- worstShop/urls.py
+++ worstShop/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('user/', include('users.urls', namespace='users')),
     path('cart/', include('cart.urls', namespace='cart')),
 ]
 

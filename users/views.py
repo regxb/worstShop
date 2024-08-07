6f88e678ea5c7ab9c47615334ff92f40274de0d6
qwe <aleksey18003@yandex.ru>
2024-08-07 21:41:26 +0300

    add users/views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from users.forms import UserCreateForm, UserAuthenticationForm
+
+User = get_user_model()
+
+
+class UserRegistrationView(generic.CreateView):
+    model = User
+    form_class = UserCreateForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('catalog:category_list')
+
+
+class UserLoginView(LoginView):
+    authentication_form = UserAuthenticationForm
+    model = User
+    template_name = 'users/login.html'
+

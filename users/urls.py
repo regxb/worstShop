fc1c2913be14651e75925ccbd12da977656edc65
qwe <aleksey18003@yandex.ru>
2024-08-07 21:41:17 +0300

    add users/urls

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.views import LogoutView
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('registration/', views.UserRegistrationView.as_view(), name='registration'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]

c1bcf346de9d888043e93819224ff0bf4cc23457
qwe <aleksey18003@yandex.ru>
2024-08-08 16:11:49 +0300

    update users/models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.mail import send_mail
+from django.conf import settings
+from django.utils.timezone import now
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_verification_email = models.BooleanField(default=False)
+
+
+class EmailVerification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='email_verification')
+    created_at = models.DateTimeField(auto_now_add=True)
+    code = models.UUIDField(unique=True)
+    expiration = models.DateTimeField()
+
+    def send_verification_email(self):
+        link = f'{settings.DOMAIN_NAME}user/verify/{self.code}'
+        send_mail(
+            subject="Subject here",
+            message=link,
+            from_email="from@example.com",
+            recipient_list=[self.user.email],
+        )
+
+    def is_expired(self):
+        return True if now() >= self.expiration else False

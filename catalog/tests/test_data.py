bff8d119dc3af2b4c7d76a65105578d6233f12ab
qwe <aleksey18003@yandex.ru>
2024-08-08 19:09:44 +0300

    update catalog/tests

--- catalog/tests/test_data.py
+++ catalog/tests/test_data.py
@@ -3,34 +3,27 @@ from catalog.models import Category, Product
 
 
 def create_test_data():
-    test_image = (
-        b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x00\x00\x00\x21\xf9\x04'
-        b'\x01\x0a\x00\x01\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'
-        b'\x02\x4c\x01\x00\x3b'
-    )
-    uploaded_file = SimpleUploadedFile('test.jpg', test_image, content_type='image/jpeg')
-
-    category_1 = Category.objects.create(name='test_1', image=uploaded_file)
-    category_2 = Category.objects.create(name='test_2', image=uploaded_file)
-    category_3 = Category.objects.create(name='test_3', image=uploaded_file)
+    category_1 = Category.objects.create(name='test_1', image='category_image1.png')
+    category_2 = Category.objects.create(name='test_2', image='category_image2.png')
+    category_3 = Category.objects.create(name='test_3', image='category_image3.png')
     product_1 = Product.objects.create(
         title='title1',
         brand='brand1',
-        image=uploaded_file
+        image='product_image1.png'
     )
     product_1.category.set([category_1])
 
     product_2 = Product.objects.create(
         title='title2',
         brand='brand2',
-        image=uploaded_file
+        image='product_image2.png'
     )
     product_2.category.set([category_1, category_2])
 
     product_3 = Product.objects.create(
         title='title3',
         brand='brand3',
-        image=uploaded_file
+        image='product_image3.png'
     )
     product_3.category.set([category_3])
 

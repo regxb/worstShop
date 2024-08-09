5fc8deb29677a7acdd1253ebb18f4c9beb2e4cdc
qwe <aleksey18003@yandex.ru>
2024-08-09 19:17:55 +0300

    update users/tests

--- users/tests/tests_route.py
+++ users/tests/tests_route.py
@@ -1,10 +1,12 @@
 from http import HTTPStatus
 
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
+from users.models import User
 
-class TestContent(TestCase):
+
+class TestAnonymousUserRoute(TestCase):
 
     def test_login_route(self):
         url = reverse('users:login')
@@ -15,3 +17,33 @@ class TestContent(TestCase):
         url = reverse('users:registration')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class TestAuthUserRoute(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='username',
+            email='email@mail.com',
+            password='testpasswordtest22'
+        )
+        self.client = Client()
+        self.client.force_login(self.user)
+
+    def test_logout_route(self):
+        url = reverse('users:logout')
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+
+    def test_auth_users_login_page(self):
+        url = reverse('users:login')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('catalog:category_list'))
+
+    def test_auth_users_register_page(self):
+        url = reverse('users:registration')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('catalog:category_list'))

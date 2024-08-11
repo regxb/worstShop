e23eac3fddd7093c2cc0e37a611f38538199b845
qwe <aleksey18003@yandex.ru>
2024-08-11 17:48:09 +0300

    add debug_toolbar

--- worstShop/urls.py
+++ worstShop/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,5 +28,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

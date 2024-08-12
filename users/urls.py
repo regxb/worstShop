1cc2d77b8f71e91e1cc46971a44cac5b05b8e77a
qwe <aleksey18003@yandex.ru>
2024-08-12 20:37:13 +0300

    update users/urls

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -8,6 +7,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('registration/', views.UserRegistrationView.as_view(), name='registration'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('verify/<uuid:code>', views.EmailVerificationView.as_view(), name='email_verification'),
 ]

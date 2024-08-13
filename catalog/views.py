5a907fc9aaf3dba9289c67892b06aacfa7ab6942
qwe <aleksey18003@yandex.ru>
2024-08-13 17:32:54 +0300

    update catalog/views

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 
 from cart.cart import Cart
 from catalog.models import Category, Product, ProductProxy
 
 
-# Create your views here.
 def products_view(request, slug):
     cart = Cart(request)
     category = get_object_or_404(Category, slug=slug)
@@ -19,10 +19,23 @@ def products_view(request, slug):
     return render(request, 'catalog/products.html', context)
 
 
-def product_detail_view(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, 'catalog/product_detail.html', {'product': product})
-
-
 def category_list(request):
     return render(request, 'catalog/category_list.html')
+
+
+def search(request):
+    cart = Cart(request)
+    query = request.GET.get('search_tag')
+    if query:
+        products = Product.objects.filter(
+            Q(title__icontains=query)
+        )
+        cart_products_title = [item['product'].title for item in cart]
+        products_title_list = [str(item) for item in products]
+        context = {
+            "products": products,
+            'products_title_list': products_title_list,
+            'cart_products_title': cart_products_title,
+        }
+        return render(request, 'catalog/products.html', context)
+    return render(request, 'catalog/blank_search.html')

d54c9026a1ff4edb74fe4561eedf97f0b72c10fe
qwe <aleksey18003@yandex.ru>
2024-08-20 23:12:21 +0300

    update orders/urls add

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ app_name = 'orders'
 urlpatterns = [
     path('order-list/', views.OrderView.as_view(), name='orders'),
     path('create/', views.OrderCreateView.as_view(), name='create'),
+    path('order-created/', views.OrderSuccessCreateView.as_view(), name='created'),
 ]

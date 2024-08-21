4f6e60709e5088eaab0553a97d40ea5509fe01e9
qwe <aleksey18003@yandex.ru>
2024-08-21 19:06:29 +0300

    update catalog/urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.category_list, name='category_list'),
     path('<slug:slug>', views.products_view, name='products'),
     path('search/', views.search, name='search'),
+    path('product-detail/<slug:slug>', views.product_details, name='detail'),
 ]

025b69d42499a08baff355717e1ac6488af88131
qwe <aleksey18003@yandex.ru>
2024-08-21 13:12:07 +0300

    update users/urls add new path

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
@@ -9,4 +10,7 @@ urlpatterns = [
     path('registration/', views.UserRegistrationView.as_view(), name='registration'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('verify/<uuid:code>', views.EmailVerificationView.as_view(), name='email_verification'),
+    path('wishlist/', login_required(views.UserWishListView.as_view()), name='wishlist'),
+    path('wishlist/add/', login_required(views.add_to_wishlist), name='wishlist_add'),
+    path('wishlist/delete/', login_required(views.delete_from_wishlist), name='wishlist_delete'),
 ]

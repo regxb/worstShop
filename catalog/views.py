8c561587265fb086a1bafc5b5b52b4b41d78c7b2
qwe <aleksey18003@yandex.ru>
2024-08-23 15:14:14 +0300

    update catalog/views add product list cache

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 
@@ -12,7 +13,12 @@ def products_view(request, slug):
         wishlist_product_title = [item.product.title for item in Wishlist.objects.filter(user=request.user)]
     cart = Cart(request.session)
     category = get_object_or_404(Category, slug=slug)
-    products = ProductProxy.objects.filter(category=category)
+
+    products = cache.get(f'products_{slug}')
+    if not products:
+        products = ProductProxy.objects.filter(category=category)
+        cache.set(f'products_{slug}', products, 10)
+
     cart_products_title = [item['product'].title for item in cart]
     products_title_list = [str(item) for item in products]
     context = {
@@ -52,7 +58,12 @@ def product_details(request, slug):
         wishlist_product_title = [item.product.title for item in Wishlist.objects.filter(user=request.user)]
     cart = Cart(request.session)
     cart_products_title = [item['product'].title for item in cart]
-    product = Product.objects.get(slug=slug)
+
+    product = cache.get(f'product_{slug}')
+    if not product:
+        product = Product.objects.get(slug=slug)
+        cache.set(f'product_{slug}', product, 300)
+
     same_products = Product.objects.filter(brand=product.brand).exclude(id=product.id)
     context = {
         'product': product,
@@ -61,4 +72,3 @@ def product_details(request, slug):
         'wishlist_product_title': wishlist_product_title,
     }
     return render(request, 'catalog/product_details.html', context)
-

3f5774cdd34a0a1b73b71b0774eae788b2c105b6
qwe <aleksey18003@yandex.ru>
2024-08-23 18:10:05 +0300

    add orders/serializers

---
+++ orders/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from orders.models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = ('initiator', 'status', 'phone_number')
+
+    def get_status(self, obj):
+        return obj.get_status_display()
+    

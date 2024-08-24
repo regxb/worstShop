47484d29db7d425300f4c1aa4efaec98d56d0a9c
qwe <aleksey18003@yandex.ru>
2024-08-24 22:41:42 +0300

    update orders/forms - add label for all fields

--- orders/forms.py
+++ orders/forms.py
@@ -5,11 +5,11 @@ from orders.models import Order
 
 class OrderForm(forms.ModelForm):
 
-    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone_number = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(label='Имя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Фамилия', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    address = forms.CharField(label='Адрес', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone_number = forms.IntegerField(label='Номер телефона', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Адрес электронной почты', widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Order

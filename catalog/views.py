0a8043faa108c49118f641e9c279d50d2c3a872f
qwe <aleksey18003@yandex.ru>
2024-08-25 16:07:13 +0300

    update catalog/views added a check, if the user is authenticated, so he has not left a review for this product, then he can leave a review

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.cache import cache
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from cart.cart import Cart
 from catalog.forms import ReviewForm
-from catalog.models import Category, Product, ProductProxy
-from orders.models import OrderItem
+from catalog.models import Category, Product, ProductProxy, Review
 from users.models import Wishlist
 
 
@@ -23,7 +20,6 @@ def products_view(request, slug):
     if not products:
         products = ProductProxy.objects.filter(category=category)
         cache.set(f'products_{slug}', products, 10)
-
     cart_products_title = [item['product'].title for item in cart]
     products_title_list = [str(item) for item in products]
     context = {
@@ -58,17 +54,17 @@ def search(request):
 
 
 def product_details(request, slug):
+    reviews = Review.objects.select_related('product').filter(product__slug=slug)
     review_form = ReviewForm()
     product = cache.get(f'product_{slug}')
     if not product:
         product = Product.objects.get(slug=slug)
         cache.set(f'product_{slug}', product, 300)
     wishlist_product_title = []
-    matching_items = False
-    if request.user.is_authenticated:
+    user_can_add_review = False
+    if request.user.is_authenticated and not reviews.filter(user=request.user).exists():
         wishlist_product_title = [item.product.title for item in Wishlist.objects.filter(user=request.user)]
-        user_oder_products = OrderItem.objects.filter(user=request.user)
-        matching_items = any(item.product == product and item.order.status != 0 for item in user_oder_products)
+        user_can_add_review = True
     cart = Cart(request.session)
     cart_products_title = [item['product'].title for item in cart]
 
@@ -78,16 +74,19 @@ def product_details(request, slug):
         'same_products': same_products,
         'cart_products_title': cart_products_title,
         'wishlist_product_title': wishlist_product_title,
-        'matching_items': matching_items,
+        'user_can_add_review': user_can_add_review,
         'review_form': review_form,
+        'reviews': reviews,
     }
     return render(request, 'catalog/product_details.html', context)
 
 
 class ReviewCreateView(CreateView):
     form_class = ReviewForm
-    success_url = reverse_lazy('catalog:category_list')
     template_name = 'catalog/product_details.html'
+
+    def get_success_url(self):
+        return self.request.path.split('/add-review')[0]
 
     def form_valid(self, form):
         product_id = self.request.POST.get('product_id')

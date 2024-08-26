bb7000e0747d79e00f864d19b95aa607b3e522da
qwe <aleksey18003@yandex.ru>
2024-08-26 13:01:43 +0300

    minor changes

--- catalog/forms.py
+++ catalog/forms.py
@@ -16,4 +16,3 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = ['rating', 'comment',]
         labels = {'rating': ''}
-
--- orders/urls.py
+++ orders/urls.py
@@ -7,5 +7,5 @@ app_name = 'orders'
 urlpatterns = [
     path('order-list/', views.OrderView.as_view(), name='orders'),
     path('create/', views.OrderCreateView.as_view(), name='create'),
-    path('order-created/', views.OrderSuccessCreateView.as_view(), name='created'),
+    path('order-status/', views.OrderAfterPaymentView.as_view(), name='created'),
 ]
